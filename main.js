9df84800ca5da6cf8109807fb09619c869b7ef07
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-17 16:22:23 +0100

    tentativo primo carousel (si interrompe alla fine)

--- main.js
+++ main.js
@@ -16,6 +16,16 @@ $(document).ready(function(){
 function clickNext(){
   var imgActive = $('.active');
   var nextImg = imgActive.next();
+
+  if(imgActive.hasClass('last') == true){
+    $('img.first').addClass('active');
+    $('img.last').removeClass('active');
+  }
+
+  else{
+    imgActive.removeClass('active');
+    nextImg.addClass('active');
+  }
 };
 
 function clickPrev(){
