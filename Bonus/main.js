efd5e86ecb6c57124cac87dc614a9911b3afad56
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-17 16:47:57 +0100

    iniziato bonus

---
+++ Bonus/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+  $('.next').click(
+    function(){
+      clickNext();
+    }
+);
+  $('.prev').click(
+    function(){
+      clickPrev();
+    }
+  );
+ }
+);
+
+function clickNext(){
+  var imgActive = $('.active');
+  var nextImg = imgActive.next();
+
+  if(imgActive.hasClass('last') == true){
+    imgActive.removeClass('active');
+    $('img.first').addClass('active');
+  }
+
+  else{
+    imgActive.removeClass('active');
+    nextImg.addClass('active');
+  }
+};
+
+function clickPrev(){
+  var imgActive = $('.active');
+  var prevImg = imgActive.prev();
+
+  if (imgActive.hasClass('first') == true){
+    imgActive.removeClass('active');
+    $('img.last').addClass('active');
+  }
+  else {
+    imgActive.removeClass('active');
+    prevImg.addClass('active');
+  }
+};
+
+$(document).keydown(
+  function(){
+    console.log(event.keyCode);
+  }
+)
